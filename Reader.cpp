6226924d77ac08fcdb2db0feb0beb1311382a4af
Henry Zhang <71099565+HenryCZhang@users.noreply.github.com>
2021-11-10 18:06:23 -0500

    go

--- Reader.cpp
+++ Reader.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "SharedObject.h"
-//using namespace std;
+#include "Semaphore.h"
 
 struct MyShared{
 	int sdelay;
@@ -9,19 +9,21 @@ struct MyShared{
 	bool sRunning;
 };
 
-
 int main(void)
 {
 	std::cout << "I am a reader" << std::endl;
-	Shared<MyShared> sharedMemory("sharedMemory");
+
+	Semaphore readerSem("readerSemaphore");
+	Shared<MyShared> sharedMemory("sharedMemory"); //ownership set to false when omitted
 	
 
 	//Reader will poll the shared memory to determine if it is in use, if it is in use
 	// then it will display the data, if it is not it will break 
 	while (true){
 		if (sharedMemory->sRunning == true){
+			readerSem.Wait();
 			std::cout << "ThreadID: " << sharedMemory->sthreadID <<" ReportID: "<<sharedMemory->sreportID <<" Delay: " <<sharedMemory->sdelay <<std::endl;
-			sleep(2); 
+		
 		}else {
 			//When a user enters n the value of Running will be set to false and the loop will break causing the reader to terminate
 			break;
--- Writer.cpp
+++ Writer.cpp
@@ -1,8 +1,8 @@
-//Created By: Andrew Brown 20/10/28
 #include <iostream>
 #include "SharedObject.h"
 #include "thread.h"
 #include <string> 
+#include "Semaphore.h"
 
 struct MyShared{
 	int sdelay;
@@ -24,19 +24,29 @@ public:
 		}
 
 		virtual long ThreadMain(void) override{
-			
+
 			Shared<MyShared> sharedMemory ("sharedMemory");
-			while(true)// Here the memory values of the thread must be updated
-			{
-				sharedMemory->sthreadID = threadID; //sets thread ID
-				sharedMemory->sreportID = reportID; //sets number of reports
-				reportID ++; //increments the number of reports 
-				sharedMemory->sdelay = delay;//sets delay
-				sleep(delay); //sleeps the thread for the amount of delay set by the user
-				if(flag){
-					break;
-				}
+			Semaphore writerSem("writerSemaphore");
+			Semaphore readerSem("readerSemaphore");//creating semaphores for the writer and reader
+			
+				// Below the memory values of the thread must be updated
+				while(true){
+					writerSem.Wait(); //Lock resource so no others can write to the shared memory at the same time
+					//Update the thread information while it is locked
+					sharedMemory->sthreadID = threadID; //sets thread ID
+					sharedMemory->sreportID = reportID; //sets number of reports
+					sharedMemory->sdelay = delay;//sets delay
+					reportID ++; //increments the number of reports 
+					
+				writerSem.Signal(); //once the information is set the resource can be unlocked for other read write operations to occur as needed
+					readerSem.Signal(); //signal the reader semaphore so that reads can occur from the shared resource 
+					
+					sleep(delay); //sleeps the thread for the amount of delay set by the user
+					if(flag){
+						break;
+					}
 			}
+			return 1;
 		}
 };
 
@@ -44,6 +54,9 @@ public:
 
 int main(void)
 {
+	Semaphore writerSem("writerSemaphore", 1, true); //Creates sem with initial value of one and owner status as true
+	Semaphore readerSem("readerSemaphore", 0, true); //Creates sem with initial value of zero and owner status as false	
+	
 	std::string  userInput;
 	std::string userDelay;
 	int numThreads=1; //Used for creating writerThread objects where numThreads will be used to give threads ID #s
@@ -51,10 +64,9 @@ int main(void)
 	std::cout << "I am a Writer" << std::endl;
 	
 	WriterThread * thread; //declare thread 
-
 	Shared<MyShared> shared("sharedMemory", true); //This is the owner of sharedMamory
-	//Set the running value to true so the reader can poll the shared memory
-	shared ->sRunning = true;
+	
+	
 
 	
 	while(true){
@@ -65,7 +77,7 @@ int main(void)
 			std::cout << "Please enter a delay time for this thread: "<< std::endl;
 			getline(std::cin,userDelay);
 			int delay = atoi(userDelay.c_str()); //Convert the string to and int
-
+			shared ->sRunning = true;//Set the running value to true so the reader can poll the shared memory
 			//Create a new writerThread object 
 			thread = new WriterThread(delay,numThreads);//instantiate thread 
 			numThreads++; //Increase the number of threads 

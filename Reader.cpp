98e47da25bdf0e445742e5fa0bc8afa120ea8e16
Henry Zhang <71099565+HenryCZhang@users.noreply.github.com>
2021-11-10 18:41:28 -0500

    go

--- Reader.cpp
+++ Reader.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "SharedObject.h"
-#include "Semaphore.h"
+//using namespace std;
 
 struct MyShared{
 	int sdelay;
@@ -9,21 +9,19 @@ struct MyShared{
 	bool sRunning;
 };
 
+
 int main(void)
 {
 	std::cout << "I am a reader" << std::endl;
-
-	Semaphore readerSem("readerSemaphore");
-	Shared<MyShared> sharedMemory("sharedMemory"); //ownership set to false when omitted
+	Shared<MyShared> sharedMemory("sharedMemory");
 	
 
 	//Reader will poll the shared memory to determine if it is in use, if it is in use
 	// then it will display the data, if it is not it will break 
 	while (true){
 		if (sharedMemory->sRunning == true){
-			readerSem.Wait();
 			std::cout << "ThreadID: " << sharedMemory->sthreadID <<" ReportID: "<<sharedMemory->sreportID <<" Delay: " <<sharedMemory->sdelay <<std::endl;
-		
+			sleep(2); 
 		}else {
 			//When a user enters n the value of Running will be set to false and the loop will break causing the reader to terminate
 			break;
